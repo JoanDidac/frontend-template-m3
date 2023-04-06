bd5244f669e4bcc897dc3b89b1e29e85ac6156dc
Joan D <joandidachr@gmail.comm>
2023-04-06 21:16:25 +0200

    Adds create new post form

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import postService from '../services/postService';
+
+function CreatePost() {
+  const [postData, setPostData] = useState({
+    title: "",
+    message: "",
+    media: "",
+    location: {
+      latitude: "",
+      longitude: "",
+    },
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name.startsWith("location.")) {
+      const locationName = name.replace("location.", "");
+      setPostData({
+        ...postData,
+        location: { ...postData.location, [locationName]: value },
+      });
+    } else {
+      setPostData({ ...postData, [name]: value });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await postService.createPost(postData);
+      alert("Post created successfully!");
+    } catch (error) {
+      console.error("Error creating post:", error);
+      alert("Error creating post, please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Create a new Post</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={postData.title}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          name="message"
+          value={postData.message}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="media">Media URL:</label>
+        <input
+          type="text"
+          name="media"
+          value={postData.media}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="location.latitude">Latitude:</label>
+        <input
+          type="number"
+          step="any"
+          name="location.latitude"
+          value={postData.location.latitude}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="location.longitude">Longitude:</label>
+        <input
+          type="number"
+          step="any"
+          name="location.longitude"
+          value={postData.location.longitude}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Create Post</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import postService from '../services/postService';
+import CreatePost from './CreatePost';
 
 function PostsList() {
   const [posts, setPosts] = useState([]);
+  const [showCreatePostForm, setShowCreatePostForm] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,20 +18,22 @@ function PostsList() {
     fetchPosts();
   }, []);
 
+  const handleCreatePostButtonClick = () => {
+    setShowCreatePostForm(!showCreatePostForm);
+  };
+
   return (
     <div>
       <h2>All Posts</h2>
+      <button onClick={handleCreatePostButtonClick}>Create new Post</button>
+      {showCreatePostForm && <CreatePost />}
       <ul>
         {posts.map(post => (
-            
-        
           <React.Fragment key={post._id}>
             <img src={post.media} alt={post.title} />
             <li>{post.title}</li>
             <li>{post.message}</li>
           </React.Fragment>
-          
-        
         ))}
       </ul>
     </div>
