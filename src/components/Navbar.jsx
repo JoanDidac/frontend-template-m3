929adeac6df802ca395eb569ca4f9dd97c616254
Joan D <joandidachr@gmail.comm>
2023-04-13 20:59:56 +0200

    Adds burger menu to Navbar Stable version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,56 @@
-import React, { useContext } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
-import logo from '../assets/logo.jpg'; 
+import React, { useContext, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import logo from "../assets/logo.jpg";
 
 export default function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
-    navigate('/');
+    navigate("/");
+  };
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleBurgerClick = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <div className='navbar'>
-      <img src={logo} alt="Logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }} />
-      <ul>
-        <li><NavLink to="/drones">Drones</NavLink></li>
-        <li><NavLink to="/posts">Posts</NavLink></li>
-        <li><NavLink to="/myprofile"> My Profile</NavLink></li>
-      </ul>
-      <input type="checkbox" id="menu-toggle" style={{ display: 'none' }} />
-      <label htmlFor="menu-toggle" className="menu-icon">
+    <div className={`navbar${menuOpen ? " menu-open" : ""}`}>
+      <img src={logo} alt="Logo" onClick={handleLogoClick} className="logo" />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/drones">Drones</NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts">Posts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/myprofile"> My Profile</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <button
+        id="menu-toggle"
+        className="menu-toggle"
+        onClick={handleBurgerClick}
+      >
         <span className="burger-menu"></span>
-      </label>
+      </button>
+  
       <div className="menu-content">
+      {user && <p>Hello {user.username}</p>}
         <div className="search-bar">
           <input type="text" placeholder="Search..." />
         </div>
-        {user && <p>Hello {user.username}</p>}
+  
         {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
+        {!isLoggedIn && <NavLink to="/signup">Sign Up</NavLink>}
         {isLoggedIn && <button onClick={() => logOutUser()}>Log out</button>}
       </div>
     </div>
-  )
-}
+  );
+}  
