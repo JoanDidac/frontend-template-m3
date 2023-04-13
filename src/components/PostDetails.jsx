d31df35536f6c928c5e0da69aa21de03e213e121
Joan D <joandidachr@gmail.comm>
2023-04-13 20:25:10 +0200

    Adds css to PostDetails

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import postService from '../services/postService';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import postService from "../services/postService";
+
+
 
 function PostDetails() {
   const { id } = useParams();
@@ -12,7 +14,7 @@ function PostDetails() {
         const response = await postService.getPost(id);
         setPost(response);
       } catch (error) {
-        console.error('Error fetching post:', error);
+        console.error("Error fetching post:", error);
       }
     };
 
@@ -24,12 +26,17 @@ function PostDetails() {
   }
 
   return (
-    <div>
-    <img src={post.media} alt={post.title} />
-    <>
-      <h1>{post.title}</h1>
-      <p>{post.message}</p>
-      </>
+    <div className="postDetails-container">
+      <div>
+        <img src={post.media} alt={post.title} />
+        <div className="content-container">
+        
+          <h1>{post.title}</h1>
+          
+          <p>{post.message}</p>
+          
+        </div>
+      </div>
     </div>
   );
 }
