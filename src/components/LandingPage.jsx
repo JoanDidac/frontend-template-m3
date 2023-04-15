c0bbc92011088d8e7af3d0e98318fcfee195f563
Joan D <joandidachr@gmail.comm>
2023-04-15 19:24:25 +0200

    Adds Loading.jsx to LandPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,20 +1,19 @@
-// components/LandingPage.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import droneService from '../services/droneService';
+import Loading from './Loading'; // Import the Loading component
 
 export default function LandingPage() {
   const [drone, setDrone] = useState(null);
+  const [loading, setLoading] = useState(true); // Add a loading state
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchDrone = async () => {
       try {
         const response = await droneService.getDrones();
-        // display hardcoded drone as the landing page drone, in the future will display the drone with more likes.
         const droneWithVideo = { ...response[0], videoUrl: 'https://res.cloudinary.com/ddcimekqb/video/upload/v1681432329/colorful-trees-fall_kbgrzl.mp4' };
         setDrone(droneWithVideo);
-      
       } catch (error) {
         console.error(error);
       }
@@ -29,27 +28,32 @@ export default function LandingPage() {
     }
   };
 
-  
+  const handleVideoLoad = () => {
+    setLoading(false); // Update the loading state when the video is loaded
+  };
 
   return (
-    drone && (
-      <div className="landing-page">
-      
-        <video
-          src={drone.videoUrl}
-          autoPlay
-          muted
-          loop
-          className="landing-video"
-        />
-        <div className="overlay">
-        <h3>Footage of the Month recorded with Drone: </h3>
-          <h2 className="drone-model">{drone.model}</h2>
-          <button onClick={handleSeeMore} className="see-more-btn">
-            Check this Drone
-          </button>
-        </div>
-      </div>
-    )
+    <div className="landing-page">
+      {loading && <Loading loading={loading} color="#123abc" size={100} />} {/* Render the Loading component when loading */}
+      {drone && (
+        <>
+          <video
+            src={drone.videoUrl}
+            autoPlay
+            muted
+            loop
+            className="landing-video"
+            onLoadedData={handleVideoLoad} // Add an event handler for when the video is loaded
+          />
+          <div className="overlay">
+            <h3>Footage of the Month recorded with Drone: </h3>
+            <h2 className="drone-model">{drone.model}</h2>
+            <button onClick={handleSeeMore} className="see-more-btn">
+              Check this Drone
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
