900c912c28e22a86bb1bb605b640477396252afe
Joan D <joandidachr@gmail.comm>
2023-04-17 11:50:27 +0200

    Styles drone detailed view and adds animations slide-up

--- src/components/DronesDetails.jsx
+++ src/components/DronesDetails.jsx
@@ -25,18 +25,18 @@ export default function DroneDetails({ drone: propDrone }) {
 
   return (
     drone && (
-      <div className="drone-details">
-        <h1 className="drone-model">{drone.model}</h1>
+      <div className="drone-details slide-up-detailed-drone">
+        <h1 className="drone-model detailed-drone-title">{drone.model}</h1>
         <img className="drone-image" src={drone.imageUrl} alt={drone.model} />
         <div className="drone-specs">
-          <p>{drone.brand}</p>
-          <p>Max Flight Time: {drone.specs?.maxFlightTime || "N/A"} minutes</p>
-          <p>Max Speed: {drone.specs?.maxSpeed || "N/A"} km/h</p>
-          <p>Range: {drone.specs?.range || "N/A"} km</p>
-          <p>Weight: {drone.specs?.weight || "N/A"} kg</p>
-          <p>Dimensions: {drone.specs?.dimensions || "N/A"}</p>
+          <h2>{drone.brand}</h2>
+          <p><strong>Max Flight Time:</strong> {drone.specs?.maxFlightTime || "N/A"} minutes</p>
+          <p><strong> Speed:</strong> {drone.specs?.maxSpeed || "N/A"} km/h</p>
+          <p><strong>Range:</strong> {drone.specs?.range || "N/A"} km</p>
+          <p><strong>Weight:</strong> {drone.specs?.weight || "N/A"} kg</p>
+          <p><strong>Dimensions: </strong>{drone.specs?.dimensions || "N/A"}</p>
         </div>
-        <button className="reviews-button">Check all reviews for this Drone</button>
+        <button className="reviews-button drone-detailed-btn">Check all reviews for this Drone</button>
         {/* add Reviews component here */}
       </div>
     )
