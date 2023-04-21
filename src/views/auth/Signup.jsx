4b7d2b07f5888ded573827e297902e62d747ec48
Joan D <joandidachr@gmail.comm>
2023-04-21 12:31:49 +0200

    Adds final styles to Signup form and title, Stable Version

--- src/views/auth/Signup.jsx
+++ src/views/auth/Signup.jsx
@@ -65,39 +65,38 @@ export default function Signup() {
   }
 
   return (
-   
     <div className='login-page login-form-container ' >
-    <div className='signup-title'>
-   <h1 className='metal-gradient'> Welcome to Sky Pulse</h1>
-   
-     <video
-        className='background-video background-video-signup'
-        autoPlay
-        loop
-        muted
-      >
-        <source
-          src="https://res.cloudinary.com/ddcimekqb/video/upload/v1681432281/city-timelapse_bg04ol.mp4"
-          type="video/mp4"
-        />
-      </video>
-      
-      <form className='login-form' onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input required type="text" name="username" value={user.username} onChange={handleChange} />
-        <label>Email</label>
-        <input required type="email" name="email" value={user.email} onChange={handleChange} />
-        <label>Password</label>
-        <input required type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value) } />
-        <label>Repeat the password</label>
-        <input required type="password" name="passwordControl" value={passwordControl} onChange={(e) => setPasswordControl(e.target.value)} />
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        <button type="submit">Register</button>
-        <a href='/login'>Alredy have an account? <strong>☞ Login</strong></a>
-      </form>
-    </div>
-    </div>
+      <div className='signup-title'>
+       <div className='electricity-container'></div>
+       
+        <h1 className='electricity-text'>Welcome to Sky Pulse</h1>
+        <video
+          className='background-video background-video-signup'
+          autoPlay
+          loop
+          muted
+        >
+          <source
+            src="https://res.cloudinary.com/ddcimekqb/video/upload/v1681432281/city-timelapse_bg04ol.mp4"
+            type="video/mp4"
+          />
+        </video>
 
+        <form className='login-form' onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input required type="text" name="username" value={user.username} onChange={handleChange} />
+          <label>Email</label>
+          <input required type="email" name="email" value={user.email} onChange={handleChange} />
+          <label>Password</label>
+          <input required type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <label>Repeat the password</label>
+          <input required type="password" name="passwordControl" value={passwordControl} onChange={(e) => setPasswordControl(e.target.value)} />
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <button type="submit">Register</button>
+          <a href='/login'>Already have an account? <strong>☞ Login</strong></a>
+        </form>
+      </div>
+    </div>
 
   )
 }
